a5dd53376a0ee8ec60d05084a54015df25f1f49a
Ray Liu <rayliu0712@gmail.com>
2024-11-21 23:35:41 +0800

    v2

--- core.h
+++ core.h
@@ -5,11 +5,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define Curses_End(void) endwin(void)
-#define Curses_Clear(void) clear(void)
-#define Curses_GetChar(void) getch(void)
+typedef const char *Fmt;
 
-void Curses_Init() {
+#define Cur_Y (stdscr->_cury)
+#define Cur_X (stdscr->_curx)
+
+#define Cur_End() endwin()
+#define Cur_Clear() clear()
+#define Cur_GetChar() getch()
+
+void Cur_Init(void) {
 	initscr();
 	keypad(stdscr, true);
 	cbreak();
@@ -17,54 +22,71 @@ void Curses_Init() {
 	curs_set(0);
 }
 
-void Curses_Print(const char *fmt, ...) {
+void Cur_Print(Fmt fmt, ...) {
 	va_list args;
 	va_start(args, fmt);
+
 	vw_printw(stdscr, fmt, args);
+	refresh();
+
 	va_end(args);
 }
 
-void Curses_Move_Print(int y, int x, const char *fmt, ...) {
+void Cur_Move_Print(int y, int x, Fmt fmt, ...) {
 	va_list args;
 	va_start(args, fmt);
+
 	move(y, x);
 	vw_printw(stdscr, fmt, args);
+	refresh();
+
 	va_end(args);
 }
 
-void _Curses_V_PrintCenter(const char *fmt, va_list args) {
-	char *s = (char *)calloc(COLS * 2, sizeof(char));
-	vsprintf(s, fmt, args);
-	char **lines = (char **)calloc(LINES * 2, sizeof(char *));
+#define _Cur_CENTER -1
+#define _Cur_CLEAR -2
 
+#define Cur_NewCenter(fmt, ...) _Cur_PrintCenterOpt(_Cur_CLEAR, _Cur_CLEAR, (fmt), ##__VA_ARGS__)
+#define Cur_PrintCenter(fmt, ...) _Cur_PrintCenterOpt(_Cur_CENTER, _Cur_CENTER, (fmt), ##__VA_ARGS__)
+
+#define Cur_Print_Xmid(fmt, ...) _Cur_PrintCenterOpt(Cur_Y, _Cur_CENTER, (fmt), ##__VA_ARGS__)
+#define Cur_Move_Print_Xmid(y, fmt, ...) _Cur_PrintCenterOpt((y), _Cur_CENTER, (fmt), ##__VA_ARGS__)
+
+#define Cur_Print_Ymid(fmt, ...) _Cur_PrintCenterOpt(_Cur_CENTER, Cur_X, (fmt), ##__VA_ARGS__)
+#define Cur_Move_Print_Ymid(x, fmt, ...) _Cur_PrintCenterOpt(_Cur_CENTER, (x), (fmt), ##__VA_ARGS__)
+
+void _Cur_PrintCenterOpt(int y, int x, Fmt fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+
+	char *s, **lines;
 	int n = 0;
-	char *tok = strtok(s, "\n");
-	for (; tok != NULL; n++) {
-		lines[n] = tok;
-		tok = strtok(NULL, "\n");
+	// split by lines
+	{
+		s = (char *)calloc(COLS * 2, sizeof(char));
+		vsprintf(s, fmt, args);
+		lines = (char **)calloc(LINES * 2, sizeof(char *));
+
+		char *tok = strtok(s, "\n");
+		for (; tok != NULL; n++) {
+			lines[n] = tok;
+			tok = strtok(NULL, "\n");
+		}
 	}
 
-	int shift = (LINES - n) / 2;
+	if (y == _Cur_CLEAR && x == _Cur_CLEAR) {
+		Cur_Clear();
+		y = x = _Cur_CENTER;
+	}
+
 	for (int i = 0; i < n; i++) {
 		char *line = lines[i];
-		Curses_Move_Print(shift + i, (COLS - strlen(line)) / 2, line);
+		int yy = (y == _Cur_CENTER) ? ((LINES - n) / 2 + i) : (y + i);
+		int xx = (x == _Cur_CENTER) ? ((COLS - strlen(line)) / 2) : x;
+		Cur_Move_Print(yy, xx, line);
 	}
 
 	free(s);
 	free(lines);
-}
-
-void Curses_PrintCenter(const char *fmt, ...) {
-	va_list args;
-	va_start(args, fmt);
-	_Curses_V_PrintCenter(fmt, args);
-	va_end(args);
-}
-
-void Curses_NewCenter(const char *fmt, ...) {
-	va_list args;
-	va_start(args, fmt);
-	Curses_Clear();
-	_Curses_V_PrintCenter(fmt, args);
 	va_end(args);
 }
--- demo.c
+++ demo.c
@@ -1,32 +1,32 @@
 #include "core.h" // 必須以 "" 而非 <> include core.h 標頭檔
 
 int main() {
-	Curses_Init(); // 初始化Curses，必須放在最前面
+	Cur_Init(); // 初始化Curses，必須放在最前面
 
 	int month = 11, day = 21;
-	Curses_Move_Print(0, 0, "Today is %d/%d, Press Enter to continue", month, day);
+	Cur_Move_Print(0, 0, "Today is %d/%d, Press Enter to continue", month, day);
 	// 第一個參數傳入y(第y行)
 	// 第二個參數傳入x(第x列)
 	// 第三個參數之後用法和printf相同
 
 	while (1) {
-		int key = Curses_GetChar(); // 取得使用者按下哪個鍵
+		int key = Cur_GetChar(); // 取得使用者按下哪個鍵
 		if (key == '\n' || key == KEY_ENTER)
 			break;
 	}
 
-	Curses_Print("%d-%d=%d", month, day); // 照上次的座標繼續輸出
+	Cur_Print("%d-%d=%d", month, day); // 照上次的座標繼續輸出
 	// Curses的所有Print函數都支援以printf的方式輸出
 
-	Curses_Clear(); // 清除螢幕
+	Cur_Clear(); // 清除螢幕
 
-	Curses_PrintCenter("CENTER 1\nPress Any Key to continue"); // 輸出在正中間(支援多行)
+	Cur_PrintCenter("CENTER 1\nPress Any Key to continue"); // 輸出在正中間(支援多行)
 
-	Curses_GetChar();
+	Cur_GetChar();
 
-	Curses_NewCenter("CENTER 2\nPress Any Key to continue"); // 清除 + 輸出在正中間
+	Cur_NewCenter("CENTER 2\nPress Any Key to continue"); // 清除 + 輸出在正中間
 
-	Curses_GetChar();
+	Cur_GetChar();
 
-	Curses_End(); // 清除Curses，必須放在最後面
+	Cur_End(); // 清除Curses，必須放在最後面
 }
